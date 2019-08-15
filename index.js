3128929522a64cd7fd855c7f7be2bb0acc67bf50
github username <github email address>
2019-08-15 13:43:47 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Your code here
+function saturdayFun(activity='roller-skate') {
+    let string = `This Saturday, I want to ${activity}!`
+    console.log(`This Saturday, I want to ${activity}!`)
+    return string
+}
+
+function mondayWork(activity = "go to the office") {
+    let string = `This Monday, I will ${activity}.`
+    return string
+}
+
+function wrapAdjective(highlight = "*") {
+    
+    return function wrap(parameter ="special"){
+        let string = `You are ${highlight}${parameter}${highlight}!`
+        return string
+    }
+}
+
+let Calculator = {
+    add: function add(a,b){return a+b},
+    subtract: function subtract(a,b) {return a-b},
+    multiply: function multiply(a,b) {return a*b},
+    divide: function divide(a,b) {return a/b}
+}
+
+function actionApplyer(integer=0, array) {
+    let result = array.length ? (((integer*2)+1000)%7) : integer
+    return result
+}
